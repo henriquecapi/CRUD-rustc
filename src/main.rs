bffb1e1d360eb0629341b492a5678d38a1670895
henriquecapi <henriquecapi@hotmail.com>
2025-07-26 20:52:58 -0300

    cargo run ok. Initial Bugs correct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Marca a função main com asyncrona e usa o runtime async-std
-#[async-std::main]
+#[async_std::main]
 async fn main() -> tide::Result<()> {
     // Define o endereço a porta onde a API vai rodar
     let addr = "127.0.0.1:8080";
@@ -11,12 +11,12 @@ async fn main() -> tide::Result<()> {
 
     // Define uma Rota Get para o caminho raiz ("/")
     // Quando a requisição Get chega em "/", ela responde com "Hello World!"
-    app.at("/").get(|.| async {
-        Ok("Hello World!")
+    app.at("/").get(|_| async {
+        Ok("Hello, world!")
     });
 
     // Inicia o servidor Tide e o faz escutar as requisições no endereço definido
-    app.Listen(addr).await?;
+    app.listen(addr).await?;
 
     // Retorna vazio (sucesso)
     Ok(())
