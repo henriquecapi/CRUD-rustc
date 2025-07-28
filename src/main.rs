38d059407ebdec38f5a4cf47670a54c52db6f111
Henrique Capibaribe <henriquecapi@hotmail.com>
2025-07-28 13:59:58 -0300

    Update to Deploy Railway.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crud_henriquecapi::setup_app;
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let app = setup_app();
-    let addr = "127.0.0.1:8080";
+    let addr = "0.0.0.0:8080";
     println!("Servidor CRUD rodando em: http://{addr}");
     app.listen(addr).await?;
     Ok(())
